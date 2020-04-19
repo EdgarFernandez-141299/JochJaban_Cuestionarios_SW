14d6ad55ad2612a8c6f2f8d9ba84bec11032d398
EdgarFernandez-141299 <edgarfernavila@gmail.com>
2020-04-18 22:42:19 -0500

    first commit

---
+++ db/db_Cuestionarios.sql
@@ -0,0 +1,130 @@
+create database db_Cuestionarios;
+use db_Cuestionarios;
+
+create table Usuario(
+nombre_Usuario varchar(200) not null,eee 
+password_Usuario varchar(300) not null,
+tipo_Usuario varchar(300) not null,
+nombres_Trabajador varchar(300) not null,
+apellidoPat_Trabajador varchar(300) not null,
+apellidoMat_Trabajador varchar(300) not null,
+genero_Usuario varchar(200)not null,
+estado_Civil varchar(100) not null,
+escolaridad varchar(200) not null,
+centro_Trabajo varchar(300) not null,
+depto_area varchar(300) not null,
+puesto_actividad varchar(300) not null,
+antiguedad_anios bigint not null,
+antiguedad_meses int not null,
+experiencia_anios bigint not null,
+experiencia_meses int not null,
+tipo_Puesto varchar(200) not null,
+tipo_Contratacion varchar(200) not null,
+tipo_Personal varchar(200) not null,
+tipo_Jornada varchar(100) not null,
+rola_turnos varchar(100) not null,
+CONSTRAINT PK_nomUsu PRIMARY KEY (nombre_Usuario))
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table CuestFRP(
+id_CuestionarioFRP varchar(200) not null,
+cat_AmbienteTrabajoFRP bigint not null,
+cat_FactoresPropActivFRP bigint not null,
+cat_OrganiTiempTrabFRP bigint not null,
+cat_LiderazgoRelaTrabFRP bigint not null,
+dom_CondicioAmbTrabFRP bigint not null,
+dom_CargaTrabFRP bigint not null,
+dom_FaltaControlTrabFRP bigint not null,
+dom_JornaTrabFRP bigint not null,
+dom_InterfeRelaTrabFamFRP bigint not null,
+dom_LiderazgoFRP bigint not null,
+dom_RelaTrabFRP bigint not null,
+dom_ViolenciaFRP bigint not null,
+calif_CatFRP bigint not null,
+calif_DomFRP bigint not null,
+CONSTRAINT PK_idCuesFRP PRIMARY KEY (id_CuestionarioFRP))
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+create table CuestFRPEO(
+id_CuestionarioFRPEO varchar(200) not null,
+cat_AmbienteTrabajoFRPEO bigint not null,
+cat_FactoresPropActivFRPEO bigint not null,
+cat_OrganiTiempTrabFRPEO bigint not null,
+cat_LiderazgoRelaTrabFRPEO bigint not null,
+cat_EntornoOrganFRPEO bigint not null,
+dom_CondicioAmbTrabFRPEO bigint not null,
+dom_CargaTrabFRPEO bigint not null,
+dom_FaltaControlTrabFRPEO bigint not null,
+dom_JornaTrabFRPEO bigint not null,
+dom_InterfeRelaTrabFamFRPEO bigint not null,
+dom_LiderazgoFRPEO bigint not null,
+dom_RelaTrabFRPEO bigint not null,
+dom_ViolenciaFRPEO bigint not null,
+dom_ReconocimientoDesempFRPEO bigint not null,
+dom_InsuficientePertenenciaInestaFRPEO bigint not null,
+calif_CatFRPEO bigint not null,
+calif_DomFRPEO bigint not null,
+CONSTRAINT PK_idCuesFRPEO PRIMARY KEY (id_CuestionarioFRPEO))
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table CuestATS(
+id_CuestionarioATS varchar(200) not null,
+
+
+CONSTRAINT PK_idCuesATS PRIMARY KEY (id_CuestionarioATS))
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table Empresa(
+id_Empresa varchar(200) not null,
+nombre_Empresa varchar(500) not null,
+capacidad_Empresa bigint not null,
+CONSTRAINT PK_idEmpresa PRIMARY KEY (id_Empresa))
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table FRP_USUA(
+id_CuestionarioFRP1 varchar(200) not null,
+nombre_Usuario1 varchar(200) not null,
+fecha_AplicacionFRP datetime not null,
+CONSTRAINT FK_idCuestFRP1 FOREIGN KEY (id_CuestionarioFRP1) REFERENCES CuestFRP(id_CuestionarioFRP)on delete cascade,
+CONSTRAINT FK_nomUsu1 FOREIGN KEY (nombre_Usuario1) REFERENCES Usuario (nombre_Usuario)on delete cascade)
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table FRPEO_USUA(
+id_CuestionarioFRPEO1 varchar(200) not null,
+nombre_Usuario2 varchar(200) not null,
+fecha_AplicacionFRPEO datetime not null,
+CONSTRAINT FK_idCuestFRPEO1 FOREIGN KEY (id_CuestionarioFRPEO1) REFERENCES CuestFRPEO(id_CuestionarioFRPEO)on delete cascade,
+CONSTRAINT FK_nomUsu2 FOREIGN KEY (nombre_Usuario2) REFERENCES Usuario (nombre_Usuario)on delete cascade)
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table ATS_USUA(
+id_CuestionarioATS1 varchar(200) not null,
+nombre_Usuario3 varchar(200) not null,
+fecha_AplicacionATS datetime not null,
+CONSTRAINT FK_idCuestATS1 FOREIGN KEY (id_CuestionarioATS1) REFERENCES CuestATS(id_CuestionarioATS)on delete cascade,
+CONSTRAINT FK_nomUsu3 FOREIGN KEY (nombre_Usuario3) REFERENCES Usuario (nombre_Usuario)on delete cascade)
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table EMPRESA_USU(
+id_Empresa1 varchar(200) not null,
+nombre_Usuario4 varchar(200) not null,
+CONSTRAINT FK_idEmpresa1 FOREIGN KEY (id_Empresa1) REFERENCES Empresa(id_Empresa)on delete cascade,
+CONSTRAINT FK_nomUsu4 FOREIGN KEY (nombre_Usuario4) REFERENCES Usuario (nombre_Usuario)on delete cascade)
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ php/Login.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+     <title>Instituto Joch Jaban - Cuestionarios</title> 
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+     <script src="https://kit.fontawesome.com/d73fdf4074.js" crossorigin="anonymous"></script>
+     <script src="../js/jquery-3.5.0.min.js" type="text/javascript"></script>   
+</head>
+<body background="../images/fondoLogin.jpg">
+  <br>
+  <br>
+<div class="container pt-1" >
+  <div class="row justify-content-sm-center">
+    <div class="col-sm-10 col-md-6">
+      <div class="card border-info">	  
+        <div class="card-body" >
+		<h1><center><div class="card-header"><img src="../images/logo-joch-jaban.png"></div></center></h1>
+			<br>
+			<center><img src="../images/usuariosIcono.png" width="190px" height="100px"></center>
+			<br>  
+          <input id="usuario" type="text" class="form-control mb-2" maxlength="100"  placeholder="Ingrese el usuario" pattern="[A-Za-z0-9]+"  required autofocus/>
+          <br>
+          <input id="password" type="password"  maxlength="40" class="form-control mb-2" placeholder="Ingrese el password" required />
+          <br>
+         <center><button type="button" class ="btn btn-lg btn-info"  style="width:200px; background-color: rgb(41, 152, 164)">
+		 <i class="fas fa-sign-in-alt"> Iniciar Sesión</i> 		 
+		 </button></center>
+		 <br>
+		 <label>¿Aún no tienes cuenta? <a href="https://es-la.facebook.com/">Registrate</a></label>
+          <br>
+        </div>
+       </div>
+      </div>
+     </div>
+    </div>
+</body>
+</html>
+     
+
+
