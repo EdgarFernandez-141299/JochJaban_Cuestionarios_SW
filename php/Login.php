2087e1995de901ed41ef5f1cf1db5774141df853
EdgarFernandez-141299 <edgarfernavila@gmail.com>
2020-04-22 17:28:51 -0500

    Cambios en Login

--- php/Login.php
+++ php/Login.php
@@ -2,7 +2,7 @@
 <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-     <title>Instituto Joch Jaban - Cuestionarios</title> 
+     <title>Cuestionarios - Instituto Joch Jaban</title> 
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
      <script src="https://kit.fontawesome.com/d73fdf4074.js" crossorigin="anonymous"></script>
      <script src="../js/jquery-3.5.0.min.js" type="text/javascript"></script>   
